73ff8b0e53003e92df15596b84aea8a6e0f2871a
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-17 01:03:06 +0200

    LinkingBetweenRoutes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
     return (
-        <div>
-            WorldWise
-        </div>
-    )
+      <div>
+        <PageNav/>
+        <h1>WorldWise</h1>
+        <Link to='./Pricing'>Pricing</Link>
+      </div>
+    );
 }
 
 export default Homepage
