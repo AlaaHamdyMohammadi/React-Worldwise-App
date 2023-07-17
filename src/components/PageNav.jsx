a26d8cfd8f5f31acbe4833f98f5c7c2c2d5369be
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-17 23:57:33 +0200

    BuildingPage1

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,13 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from './PageNav.module.css';
+import Logo from './../components/Logo';
 
 function PageNav() {
     return (
       <nav className={styles.nav}>
+        <Logo />
         <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
           <li>
             <NavLink to="/pricing">Pricing</NavLink>
           </li>
@@ -15,7 +14,7 @@ function PageNav() {
             <NavLink to="/product">Product</NavLink>
           </li>
           <li>
-            <NavLink to="/app">AppLayout</NavLink>
+            <NavLink to="/login">Login</NavLink>
           </li>
         </ul>
       </nav>
