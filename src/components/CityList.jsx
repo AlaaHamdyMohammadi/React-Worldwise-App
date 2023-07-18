6a4783cc6c022cb5e9be174d8524c5ea2dc2b6fc
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-18 20:01:41 +0200

    CitiesList

---
+++ src/components/CityList.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if(!cities.length) return <Message message='Add your first city before by clicking on a city on the map'/>
+
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
